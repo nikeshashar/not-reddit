e2bcb36a0776fa02393d01a1e5a735933d251698
Toan Nguyen <toan@graduate.uwa.edu.au>
2014-08-22 13:40:32 +0100

    added username validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
+  attr_accessor :login
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username, length: { minimum: 4 }, format: { with: /\A[A-Za-z]/ }
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,9 @@
 
 FactoryGirl.define do
   factory :user do
+    username 'bob_github'
+    email 'bob@github.com'
+    password '12345678'
+    password_confirmation '12345678'
   end
 end
