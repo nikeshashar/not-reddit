217bfabe2a34b520a9f8573970f13a718b7a7404
Toan Nguyen <toan@graduate.uwa.edu.au>
2014-08-23 23:47:55 +0100

    feature added: post knows its own link

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
   validates :title, length: { minimum: 3 }, format: { with: /\A[A-Za-z]/ }
+
+  def link
+    return "/posts/#{id}" if url.empty?
+    url
+  end
 end
