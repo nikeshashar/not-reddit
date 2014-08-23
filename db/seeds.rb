bf32f1f4b734d1722c4674166bcc035771b25d15
Toan Nguyen <toan@graduate.uwa.edu.au>
2014-08-24 00:27:21 +0100

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+Post.destroy_all
+Comment.destroy_all
+
+users = []
+post_titles = %w{Nandos JayZ England Poker Australia BBC}
+urls = ['', '', 'http://nandos.co.uk', 'http://google.com', 'http://bbc.co.uk']
+statements = ['', '', 'Hipstas are taking over', 'Today is cray cray!']
+
+users << User.create(username: 'bobgithub', email: 'bob@github.com', password: '12345678', password_confirmation: '12345678')
+users << User.create(username: 'annagithub', email: 'anna@github.com', password: '12345678', password_confirmation: '12345678')
+users << User.create(username: 'miahi', email: 'miahi@github.com', password: '12345678', password_confirmation: '12345678')
+
+10.times do 
+  user = users.sample
+  Post.create(title: post_titles.sample, url: urls.sample, user_id: users.sample.id, text: statements.sample)
+end
