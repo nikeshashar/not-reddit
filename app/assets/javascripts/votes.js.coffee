1b265abff6e48ad9af3479526c949c085386dfd8
Toan Nguyen <toan@graduate.uwa.edu.au>
2014-08-24 22:04:00 +0100

    feature added: comments need to be 1 character or more

--- app/assets/javascripts/votes.js.coffee
+++ app/assets/javascripts/votes.js.coffee
@@ -4,9 +4,12 @@
 $ ->
   $('.votes').on('click', 'a', (event) ->
     event.preventDefault()
+    selection = $(this)
+    selection.siblings().removeClass('selected')
     post_score = $(this).siblings('.vote-score')
     $.post(this.href, (response) ->
       post_score.text(response.post_score)
+      selection.addClass('selected')
       ).fail((response) ->
         document.location.href = "/users/sign_in"
       )
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Comment, :type => :model do
+  let(:user) { create(:user) }
+  let(:post) { create(:post, user: user) }
 
+  it 'is valid if it has a user and post and body is greater than 1 character' do
+    comment = Comment.create(user_id: user.id, post_id: post.id, body: 'this sucks')
+    expect(comment).to be_valid
+  end
+
+  it 'is invalid if body is < 1 character' do
+    comment = Comment.create(user_id: user.id, post_id: post.id, body: '')
+    expect(comment).to have(1).error_on(:body)
+  end
 end
