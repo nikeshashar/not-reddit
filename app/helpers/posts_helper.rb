b15de0c6d9e7f9fa96d17b78f4ef73fa322f8370
Toan Nguyen <toan@graduate.uwa.edu.au>
2014-08-24 22:55:53 +0100

    feature added: html display if user has selected up or down vote

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,4 +3,8 @@ module PostsHelper
     return 'no comments' if number < 1
     pluralize(number, 'comment')
   end
+
+  def add_voting_colours(has_voted, original)
+    has_voted ? original + ' selected' : original
+  end
 end
